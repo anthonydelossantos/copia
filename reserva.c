13837c7c6862c1de2712f6321775cff9d40c457e
Tony <anthonytony2000.04@gmail.com>
2022-12-08 21:51:48 -0500

    lol

--- reserva.c
+++ reserva.c
@@ -1,15 +1,12 @@
+/*Moises Delgado 8-1001-1046
+Ashley Bulgin 8-971-1775
+Grupo 1SF113
+*/
 /*Reservación de computadoras para el laboratorio de programación*/
-//as
-#include <stdio.h>
 
-void listarEstudiantes(char nombresEstudiantes[], int cedulasEstudiantes[], int i ){
-     for (i = 0; i < 20; i++)
-                {
-                    printf("Nombre:%s ", nombresEstudiantes[i]); 
-                    printf(" Cedula:%d", cedulasEstudiantes[i]);
-                    printf("\n");
-                }
-}
+#include<stdio.h>
+#include<conio.h>
+
 void habilitarPC(int pcID, int computadoras[],int i){
     printf("Introduzca computadora a habilitar -> ");
             scanf("%d",&pcID);
@@ -70,15 +67,7 @@ void mostrarPCDisponible(int count, int computadoras[], int i, int t){
                }
             
 }
-void historial(char nombresEstudiantes[],int registroEstudiante[] , int i){
-    for (i = 0; i < 20; i++)
-                {
-                    printf("Nombre:%s ", nombresEstudiantes[i]); 
-                    printf(" Veces que ha utilizado el Laboratorio:%d", registroEstudiante[i]);
-                    printf("\n");
-                }
 
-}
 
 void reservarPC(int cedulaEstudiante, int computadoras[], int registroEstudiante[], int reserva, int cedulasEstudiantes[], int i ){
     //reservar una computadora
@@ -108,7 +97,7 @@ int main( )
 {
     int computadoras[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     int condicion = 0;
-    int cedulasEstudiantes[20] = {89541843,81027954,91341234,89541843,81028954,91141234,89541343,81027954,91341232,89541843,41341234,39585063,810271424,41341234,89540933,81037954,91341234,89543543,81297954,39611559}; 
+    int cedulasEstudiantes[20] = {89541843,81027954,89711775,89541843,81028954,91141234,810011046,81027954,91341232,89541843,41341234,39585063,810271424,41341234,89540933,81037954,91341234,89543543,81297954,39611559}; 
     char nombresEstudiantes[20][12] = {"Raul","Pedro","Ashley","Luis","Luisa","Ernesto","Moises","Lucas","Jubencio","Christopher","Samuel","Jesus","Jordan","Romulo","Gabriel","Virgilio","Eugenio","Laura","Monica","Cristel"};
     int registroEstudiante[20] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     int count = 0;
@@ -116,6 +105,7 @@ int main( )
     int reserva;
     int i,t;
     int pcID;
+    char limpiar;
     while(condicion < 1) {
         int opcion;
         printf("\nMenu: \n1) Ver los puestos de las computadoras ocupadas y disponibles.\n2) Reservar una computadora\n3) Salir del laboratorio\n4) Registro del uso del laboratorio\n5) Lista de estudiantes Autorizados\n6) Salir del sistema\n");
@@ -128,8 +118,6 @@ int main( )
 
             case 2:
             reservarPC(cedulaEstudiante,computadoras,registroEstudiante,reserva,cedulasEstudiantes,i);
-            
-          
             break;
 
             case 3:
@@ -138,16 +126,26 @@ int main( )
             break;
 
             case 4:
-            historial(nombresEstudiantes, registroEstudiante, i);
+            for (i = 0; i < 20; i++)
+                {
+                    printf("Nombre:%s ", nombresEstudiantes[i]); 
+                    printf(" Veces que ha utilizado el Laboratorio:%d", registroEstudiante[i]);
+                    printf("\n");
+                }
 
             break;
 
             case 5:
             //muestra lista de estudiantes en el sistema
-                listarEstudiantes(nombresEstudiantes,cedulasEstudiantes,i);
+                for (i = 0; i < 20; i++)
+                {
+                    printf("Nombre:%s ", nombresEstudiantes[i]); 
+                    printf(" Cedula:%d", cedulasEstudiantes[i]);
+                    printf("\n");
+                }
                 break;
 
-            case 6:
+            case 6: 
             condicion = 5;
             break;
 
@@ -155,7 +153,18 @@ int main( )
             default:
             printf("ERROR: Usted ha introducido un valor que no pertenece a este menu.");
         }
+        printf("Desea limpiar pantalla?.S/N \n");
+        scanf("%s",&limpiar);
+        if (limpiar== 's')
+        {
+            clrscr();
+        }
+        
+
     }
+    
+    
+    return 0;
 
     
 }
